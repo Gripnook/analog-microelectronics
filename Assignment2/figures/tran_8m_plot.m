465b8faa1bdb5fbf79058dd1f42e3089548a07f4
Gripnook <XxAndreixX@hotmail.com>
2017-12-16 23:45:47 -0500

    Added assignment 2

---
+++ Assignment2/figures/tran_8m_plot.m
@@ -0,0 +1,17 @@
+clear all; close all; clc;
+
+%% Read data from CSV in the same folder
+M = csvread('tran_8m.csv',1,0);
+t = M(:,1).*1000;
+vin = M(:,2);
+vout = M(:,4);
+
+%% Generate plot
+plot(t,vin,t,vout);
+xlabel('Time (ms)');
+ylabel('V (V)');
+axis([min(t),max(t),min(vin),max(vin)]);
+legend('Vin','Vout');
+grid on;
+
+saveas(gcf,'tran_8m.eps','epsc');
